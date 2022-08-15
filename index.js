d6cb1b6d828cc0928f6be2825127c79bf276d141
1sanyo <sanyo.adonai@student.moringaschool.com>
2022-08-15 16:35:07 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+function()
+{
+    const element = document.getElementsByID('main');
+    main.removeChild();  
+
+    const newHeader = document.getElementsByTagName('h1');
+
+    newHeader = document.getElementsByID('victory');
+
+    //newHeader = document.getElementById('victory');
+    newHeader.innerHTML = "<h1>David is the Champion!</h1>"; 
+}
